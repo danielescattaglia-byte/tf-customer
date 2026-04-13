9863a939afb6b417ec7b1093e8e92650221d7a2d
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 12:02:25 +0200

    Update Git reference for app_log_setup module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "app_log_setup" {
   # Richiama il WRAPPER via Git
-  source = "git::https://github.com/danielescattaglia-byte/tf-wrapper.git?ref=1.0.0"
+  source = "git::https://github.com/danielescattaglia-byte/tf-wrapper.git?ref=2.0.0"
 
   # Passa le variabili al wrapper
   app_name = "server-web-01"
