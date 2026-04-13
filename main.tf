b34cb654284010926510938041cd5aa93fbe43fa
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 09:10:32 +0000

    lkhjkl

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "local_file" "this" {
-  filename = var.file_path
-  content  = var.content
+module "app_log_setup" {
+  # Richiama il WRAPPER via Git
+  source = "git::https://github.com/tuo-account/terraform-wrapper-file.git?ref=v2.0.0"
+
+  # Passa le variabili al wrapper
+  app_name = "server-web-01"
+  message  = "Avvio sistema completato con successo."
 }
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "file_path" {
-  type = string
-}
-
-variable "content" {
-  type = string
-}
