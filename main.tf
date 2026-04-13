5b22cbc0c0c98d2a73298ea51bbbd96f4ab8a35b
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 09:08:20 +0000

    modified

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-module "my_application_storage" {
-  # Richiama il WRAPPER tramite Git
-  source = "git::https://github.com/tuo-user/terraform-wrapper.git?ref=v2.1.0"
-
-  # Passa le variabili al wrapper
-  app_name    = "ecommerce"
-  environment = "prod"
+resource "local_file" "this" {
+  filename = var.file_path
+  content  = var.content
 }
