ee7aa943985aa0e2717b4a923ee81487dba27c0b
danielescattaglia-byte <daniele.scattaglia@dinova.one>
2026-04-13 11:53:24 +0200

    Update Git reference in app_log_setup module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "app_log_setup" {
   # Richiama il WRAPPER via Git
-  source = "git::https://github.com/tuo-account/terraform-wrapper-file.git?ref=v2.0.0"
+  source = "git::https://github.com/tuo-account/terraform-wrapper-file.git?ref=1.0.0"
 
   # Passa le variabili al wrapper
   app_name = "server-web-01"
